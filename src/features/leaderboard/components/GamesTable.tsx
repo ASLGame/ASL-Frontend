d85e1331b56315accca6bfe791384408609f0ad7
Yamil <yamil.irizarry@upr.edu>
2022-03-31 20:54:35 -0400

    Created general game tabs and panels functions

--- src/features/leaderboard/components/GamesTable.tsx
+++ src/features/leaderboard/components/GamesTable.tsx
@@ -2,8 +2,7 @@ import styles from "./GamesTable.module.css"
 import { useEffect, useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import { Overall } from "./components/Overall";
-import { GamePanels } from "./components/GamePanels";
-import { Game1, Game2 } from "./components/Game";
+import { GamePanels, GameTabs } from "./components/GamePanels";
 
 export function ByGames(){
     const [tabIndex, setTabIndex] = useState(0);
@@ -11,9 +10,8 @@ export function ByGames(){
         <div className={styles.container}>
             <Tabs className={styles.tab_container} selectedIndex={tabIndex} onSelect={(indx) => setTabIndex(indx)}>
                 <TabList className={styles.tab_list}>
-                    <Tab selectedClassName={styles.tab_selected} className={styles.tab}>Overall</Tab>
-                    <Tab selectedClassName={styles.tab_selected} className={styles.tab}>Game 1</Tab>
-                    <Tab selectedClassName={styles.tab_selected} className={styles.tab}>Game 2</Tab>
+                        <Tab selectedClassName={styles.tab_selected} className={styles.tab}>Overall</Tab>
+                        {GameTabs}
                 </TabList>
                 <TabPanel className={styles.tab_panel}>
                     <h2 style={{paddingBottom: "30px"}}>Highest Overall Scores</h2>
--- src/features/leaderboard/components/components/GamePanels.tsx
+++ src/features/leaderboard/components/components/GamePanels.tsx
@@ -1,13 +1,35 @@
 import styles from "../GamesTable.module.css"
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import { Game1, Game2 } from "./Game";
+import { Game } from "./Game";
 import { Fragment } from "react";
 
-export const GamePanels = [<TabPanel className={styles.tab_panel} id="Game1">
-                                <h2 style={{paddingBottom: "30px"}}>Game 1</h2>
-                                <Game1 />
-                            </TabPanel>,
-                            <TabPanel className={styles.tab_panel}>
-                                <h2 style={{paddingBottom: "30px"}}>Game 2</h2>
-                                <Game2 />
-                            </TabPanel>]
+const totalGames = 5;
+
+
+const generatePanels = () => {
+    var panels = []
+    for (var i = 0; i < totalGames; i++){
+        panels.push(
+                    <TabPanel className={styles.tab_panel}>
+                        <h2 style={{paddingBottom: "30px"}}>Game {i + 1}</h2>
+                        <Game gid={i+1} />
+                    </TabPanel>
+        );
+    };
+    return panels;
+
+}
+
+const generateTabs = () => {
+    var tabs = [];
+    for (var i = 0; i < totalGames; i++){
+        tabs.push(
+            <Tab selectedClassName={styles.tab_selected} className={styles.tab}>Game {i+1}</Tab>
+        )
+    }
+    return tabs;
+}
+
+export const GamePanels = generatePanels();
+export const GameTabs = generateTabs();
+//this works; exporting generate directly doesnt work, no idea why
