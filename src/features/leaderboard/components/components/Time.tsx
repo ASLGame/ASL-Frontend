20be3a80444a6af2a81dd8183c89e374fb46bbd1
christian alapont <christian.alapont@upr.edu>
2022-04-25 19:01:47 -0400

    fix for leaderboards render and tab spacing.

--- src/features/leaderboard/components/components/Time.tsx
+++ src/features/leaderboard/components/components/Time.tsx
@@ -1,108 +1,123 @@
-import { useAppDispatch, useAppSelector } from "../../../../app/hooks"; 
-import { selectScores, scoreState, selectYesterday, selectToday, selectWeekly, timeState } from "../../leaderboardSlice";
-import styles from "../GamesTable.module.css"
+import { useAppDispatch, useAppSelector } from "../../../../app/hooks";
+import {
+  selectScores,
+  scoreState,
+  selectYesterday,
+  selectToday,
+  selectWeekly,
+  timeState,
+} from "../../leaderboardSlice";
+import styles from "../GamesTable.module.css";
 
+export function Today() {
+  const scores = useAppSelector(selectToday);
+  const state = useAppSelector(timeState);
+  let counter = 0;
 
-export function Today(){
-    const scores = useAppSelector(selectToday);
-    const state = useAppSelector(timeState);
-    let counter = 0;
+  const allscores = () => {
+    if (scores && scores.map) {
+      return scores!.map((score) => {
+        return (
+          <div key={score.id} style={{ width: "100%" }}>
+            <div className={styles.row}>
+              <p>
+                {(counter += 1)}. {score.username}
+              </p>
+              <p>{score.name}</p>
+              <p>{score.score}</p>
+            </div>
+          </div>
+        );
+      });
+    }
+  };
 
-    const allscores = () => {
-        return scores!.map((score) => {
-            return (
-              <div key={score.id} style={{width: "100%"}}>
-                <div className={styles.row}>
-                  <p>{counter += 1}. {score.username}</p>
-                  <p>{score.name}</p>
-                  <p>{score.score}</p>
-                </div>
-              </div>
-            );
-          });
-        };
-        if (state !== "loading") {
-            return (
-              <div style={{width: "100%"}}>
-                <div className={styles.row}>
-                  <p>User</p>
-                  <p>Game</p>
-                  <p>Score</p>
-                </div>
-               {allscores()}
-              </div>
-            );
-          } else {
-            return <p>Loading...</p>
-          }
+  if (state !== "loading") {
+    return (
+      <div style={{ width: "100%" }}>
+        <div className={styles.row}>
+          <p>User</p>
+          <p>Game</p>
+          <p>Score</p>
+        </div>
+        {allscores()}
+      </div>
+    );
+  } else {
+    return <p>Loading...</p>;
+  }
 }
 
-export function Yesterday(){
-    const scores = useAppSelector(selectYesterday);
-    const state = useAppSelector(timeState);
-    let counter = 0;
+export function Yesterday() {
+  const scores = useAppSelector(selectYesterday);
+  const state = useAppSelector(timeState);
+  let counter = 0;
 
-    const allscores = () => {
-        return scores!.map((score) => {
-            return (
-              <div key={score.id} style={{width: "100%"}} >
-                <div className={styles.row}>
-                  <p>{counter += 1}. {score.username}</p>
-                  <p>{score.name}</p>
-                  <p>{score.score}</p>
-                </div>
-              </div>
-            );
-          });
-        };
-        if (state !== "loading") {
-            return (
-              <div style={{width: "100%"}}>
-                <div className={styles.row}>
-                  <p>User</p>
-                  <p>Game</p>
-                  <p>Score</p>
-                </div>
-               {allscores()}
-              </div>
-            );
-          } else {
-            return <p>Loading...</p>
-          }
+  const allscores = () => {
+    return scores!.map((score) => {
+      return (
+        <div key={score.id} style={{ width: "100%" }}>
+          <div className={styles.row}>
+            <p>
+              {(counter += 1)}. {score.username}
+            </p>
+            <p>{score.name}</p>
+            <p>{score.score}</p>
+          </div>
+        </div>
+      );
+    });
+  };
+  if (state !== "loading") {
+    return (
+      <div style={{ width: "100%" }}>
+        <div className={styles.row}>
+          <p>User</p>
+          <p>Game</p>
+          <p>Score</p>
+        </div>
+        {allscores()}
+      </div>
+    );
+  } else {
+    return <p>Loading...</p>;
+  }
 }
 
-export function Weekly(){
-    const scores = useAppSelector(selectWeekly);
-    const state = useAppSelector(timeState);
-    let counter = 0;
+export function Weekly() {
+  const scores = useAppSelector(selectWeekly);
+  const state = useAppSelector(timeState);
+  let counter = 0;
 
-    const allscores = () => {
-        return scores!.map((score) => {
-            return (
-              <div key={score.id} style={{width: "100%"}} >
-                <div className={styles.row}>
-                  <p>{counter += 1}. {score.username}</p>
-                  <p>{score.name}</p>
-                  <p>{new Date(score.date_achieved).toLocaleDateString()}</p>
-                  <p>{score.score}</p>
-                </div>
-              </div>
-            );
-          });
-        };
-        if (state !== "loading") {
-            return (
-              <div style={{width: "100%"}}>
-                <div className={styles.row}>
-                  <p>User</p>
-                  <p>Game</p>
-                  <p>Date Achieved</p>
-                  <p>Score</p>
-                </div>
-               {allscores()}
-              </div>
-            );
-          } else {
-            return <p>Loading...</p>
-          }
+  const allscores = () => {
+    return scores!.map((score) => {
+      return (
+        <div key={score.id} style={{ width: "100%" }}>
+          <div className={styles.row}>
+            <p>
+              {(counter += 1)}. {score.username}
+            </p>
+            <p>{score.name}</p>
+            <p>{new Date(score.date_achieved).toLocaleDateString()}</p>
+            <p>{score.score}</p>
+          </div>
+        </div>
+      );
+    });
+  };
+  if (state !== "loading") {
+    return (
+      <div style={{ width: "100%" }}>
+        <div className={styles.row}>
+          <p>User</p>
+          <p>Game</p>
+          <p>Date Achieved</p>
+          <p>Score</p>
+        </div>
+        {allscores()}
+      </div>
+    );
+  } else {
+    return <p>Loading...</p>;
+  }
 }
