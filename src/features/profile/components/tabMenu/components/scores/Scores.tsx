83c2112efefc50a226b7c021ed481f5fe7e0f403
jose.biescas <jose.biescas@upr.edu>
2022-05-12 19:28:10 -0400

    Modifiedf Achievements and Scores to be mobile friendly. Other minor changes

--- src/features/profile/components/tabMenu/components/scores/Scores.tsx
+++ src/features/profile/components/tabMenu/components/scores/Scores.tsx
@@ -18,6 +18,17 @@ export function Scores() {
   const [itemOffset, setItemOffset] = useState(0);
   const itemsPerPage = 8;
 
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+    window.addEventListener("resize", handleResize);
+  });
+
   useEffect(() => {
     if (newScores !== "loading" && scores && scores.length) {
       const endOffset = itemOffset + itemsPerPage;
@@ -31,54 +42,117 @@ export function Scores() {
     setItemOffset(newOffset);
   };
 
-  function Scores({
-    currentScores,
-  }: {
-    currentScores: GameScore[] | undefined;
-  }) {
-    return (
-      <>
-        {currentScores &&
-          currentScores.map((score) => (
-            <div className={styles.score_container}>
-              <div key={score.id} className={styles.scores}>
-                <div className={styles.icon}></div>
-                <div className={styles.attributes_name}>{score.name}</div>
-                <div className={styles.attributes_score}>{score.score}</div>
-                <div className={styles.attributes_date}>
-                  {new Date(score.date_achieved).toLocaleDateString()}
-                </div>
-              </div>
-              <div className={styles.line} />
-            </div>
-          ))}
-      </>
-    );
-  }
-
   if (newScores !== "loading") {
     return (
       <div className={styles.container}>
         <div className={styles.line} />
-        <Scores currentScores={currentScores} />
-        <ReactPaginate
-          nextLabel="Next"
-          pageClassName={styles.page_item}
-          pageLinkClassName={styles.page_link}
-          previousClassName={styles.page_item}
-          previousLinkClassName={styles.page_link}
-          nextClassName={styles.page_item}
-          nextLinkClassName={styles.page_link}
-          breakLabel="..."
-          breakClassName={styles.page_item}
-          breakLinkClassName={styles.page_link}
-          pageCount={pageCount}
-          onPageChange={handlePageChange}
-          containerClassName={styles.pagination}
-          activeClassName={styles.active}
-          disabledClassName={styles.disabled}
-          disabledLinkClassName={styles.disabled}
-        />
+        {width > 800
+          ? height > 1100
+            ? currentScores &&
+              currentScores!.map((score) => (
+                <div className={styles.score_container}>
+                  <div key={score.id} className={styles.scores}>
+                    <div className={styles.icon}></div>
+                    <div className={styles.attributes_name}>{score.name}</div>
+                    <div className={styles.attributes_score}>{score.score}</div>
+                    <div className={styles.attributes_date}>
+                      {new Date(score.date_achieved).toLocaleDateString()}
+                    </div>
+                  </div>
+                  <div className={styles.line} />
+                </div>
+              ))
+            : currentScores &&
+              currentScores!.map((score) => (
+                <div className={styles.score_container}>
+                  <div key={score.id} className={styles.scores}>
+                    <div className={styles.icon}></div>
+                    <div className={styles.attributes_name}>{score.name}</div>
+                    <div className={styles.attributes_score}>{score.score}</div>
+                    <div className={styles.attributes_date}>
+                      {new Date(score.date_achieved).toLocaleDateString()}
+                    </div>
+                  </div>
+                  <div className={styles.line} />
+                </div>
+              ))
+          : currentScores &&
+            currentScores!.map((score) => (
+              <div className={styles.score_container}>
+                <div key={score.id} className={styles.scores}>
+                  <div className={styles.attributes_name_small}>
+                    {score.name}
+                  </div>
+                  <div className={styles.attributes_score_small}>
+                    {score.score}
+                  </div>
+                  <div className={styles.attributes_date}>
+                    {new Date(score.date_achieved).toLocaleDateString()}
+                  </div>
+                </div>
+                <div className={styles.line} />
+              </div>
+            ))}
+        {width > 800 ? (
+          height > 1100 ? (
+            <ReactPaginate
+              nextLabel="Next"
+              pageClassName={styles.page_item}
+              pageLinkClassName={styles.page_link}
+              previousClassName={styles.page_item}
+              previousLinkClassName={styles.page_link}
+              nextClassName={styles.page_item}
+              nextLinkClassName={styles.page_link}
+              breakLabel="..."
+              breakClassName={styles.page_item}
+              breakLinkClassName={styles.page_link}
+              pageCount={pageCount}
+              onPageChange={handlePageChange}
+              containerClassName={styles.pagination}
+              activeClassName={styles.active}
+              disabledClassName={styles.disabled}
+              disabledLinkClassName={styles.disabled}
+            />
+          ) : (
+            <ReactPaginate
+              nextLabel="Next"
+              pageClassName={styles.page_item}
+              pageLinkClassName={styles.page_link}
+              previousClassName={styles.page_item}
+              previousLinkClassName={styles.page_link}
+              nextClassName={styles.page_item}
+              nextLinkClassName={styles.page_link}
+              breakLabel="..."
+              breakClassName={styles.page_item}
+              breakLinkClassName={styles.page_link}
+              pageCount={pageCount}
+              onPageChange={handlePageChange}
+              containerClassName={styles.pagination}
+              activeClassName={styles.active}
+              disabledClassName={styles.disabled}
+              disabledLinkClassName={styles.disabled}
+            />
+          )
+        ) : (
+          <ReactPaginate
+            nextLabel="Next"
+            pageClassName={styles.page_item}
+            pageLinkClassName={styles.page_link}
+            previousClassName={styles.page_item}
+            previousLinkClassName={styles.page_link}
+            nextClassName={styles.page_item}
+            nextLinkClassName={styles.page_link}
+            breakLabel="..."
+            breakClassName={styles.page_item}
+            breakLinkClassName={styles.page_link}
+            pageCount={pageCount}
+            onPageChange={handlePageChange}
+            containerClassName={styles.pagination_small}
+            activeClassName={styles.active}
+            disabledClassName={styles.disabled}
+            disabledLinkClassName={styles.disabled}
+          />
+        )}
       </div>
     );
   } else {
