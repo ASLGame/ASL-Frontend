c473e8efd16f8a98e6261634ceb468355de4d36a
AngelGO23 <ortizangelg23@gmail.com>
2023-01-13 21:03:59 -0400

    removed tflite

--- src/components/ModelCamera/components/model.ts
+++ src/components/ModelCamera/components/model.ts
@@ -1,31 +1,23 @@
 import * as tf from "@tensorflow/tfjs";
 import { Models } from "../../../types/Models";
-import {isMobile} from 'react-device-detect';
-import * as tflite from '@tensorflow/tfjs-tflite';
+import { isMobile } from 'react-device-detect';
+
 // Adds the CPU backend.
 import '@tensorflow/tfjs-backend-cpu';
 export async function loadModel() {
   let result: Models = { R_Model: undefined, L_Model: undefined };
-  
-  if(isMobile){
-    console.log('isMobile');
-    
-    tflite.setWasmPath(
-      'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-tflite@0.0.1-alpha.8/dist/'
-    );
-    result.R_Model = await tflite.loadTFLiteModel("https://tflite-asl-models.s3.amazonaws.com/R_converted_model.tflite");
-    result.L_Model = await tflite.loadTFLiteModel("https://tflite-asl-models.s3.amazonaws.com/L_converted_model.tflite");
 
-  }else{
-    console.log("isDesktop");
-    result.R_Model = await tf.loadGraphModel(
-      "https://signy-asl-models.s3.amazonaws.com/models/R_graph_model/model.json"
-    );
-    result.L_Model = await tf.loadGraphModel(
-      "https://signy-asl-models.s3.amazonaws.com/models/L_graph_model/model.json"
-    );
-  }
-  
+
+
+  console.log("isDesktop");
+  result.R_Model = await tf.loadGraphModel(
+    "https://signy-asl-models.s3.amazonaws.com/models/R_graph_model/model.json"
+  );
+  result.L_Model = await tf.loadGraphModel(
+    "https://signy-asl-models.s3.amazonaws.com/models/L_graph_model/model.json"
+  );
+
+
   return result;
 }
 
