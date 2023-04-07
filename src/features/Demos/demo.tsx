d701d053ef3a635d68034c652df67a88b2876e9e
Angel G. Ortiz <ortizangelg23@gmail.com>
2023-04-07 16:56:33 -0400

    added 3d hand

--- src/features/Demos/demo.tsx
+++ src/features/Demos/demo.tsx
@@ -7,18 +7,16 @@ import styles from './demo.module.css'
 
 const Demo = () => {
     return (
-
-        <Suspense fallback={null}>
-            <Canvas className={styles.canvas3D}>
-                <ambientLight />
-                <pointLight position={[5, 4, -1]} intensity={1} />
-                <pointLight position={[-3, -3, 2]} />
-
-                <Model />
-
-
-            </Canvas>
-        </Suspense>
+        <div className={styles.canvas3D}>
+            <Suspense fallback={null}>
+                <Canvas >
+                    <ambientLight />
+                    <pointLight position={[5, 4, -1]} intensity={1} />
+                    <pointLight position={[0, 0, 0]} />
+                    <Model />
+                </Canvas>
+            </Suspense>
+        </div>
 
 
     )
