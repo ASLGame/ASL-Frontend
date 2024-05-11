adfa4c0dc6cd9de298213bb6b7380f4519402bff
Angel G. Ortiz <ortizangelg23@gmail.com>
2024-05-11 09:53:38 -0400

    added footer to the application. Directing it to our X account

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'; // Corrected icon import
 
-const footer = () => {
+const Footer = () => {
   return (
-    <div>footer</div>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: '10px 20px',
+      background: '#f0f0f0',
+      borderTop: '1px solid #e1e1e1'
+    }}>
+      
+      <a href="https://x.com/SignyHands" target="_blank" rel="noopener noreferrer" style={{ color: '#1DA1F2', display: 'flex', alignItems: 'center' }}>
+        <FontAwesomeIcon icon={faTwitter} style={{ marginRight: '5px' }} />
+        Contact Us
+      </a>
+    </div>
   )
 }
 
-export default footer
+export default Footer
