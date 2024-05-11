1bcf77960e42de4183ac714476e467713bae9ec7
Angel G. Ortiz <ortizangelg23@gmail.com>
2024-05-11 11:12:08 -0400

    footer adjust

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -6,14 +6,15 @@ const Footer = () => {
   return (
     <div style={{
       display: 'flex',
-      justifyContent: 'space-between',
+      justifyContent: 'flex-end',
       alignItems: 'center',
+      height: '50px',
       padding: '10px 20px',
       background: '#f0f0f0',
       borderTop: '1px solid #e1e1e1'
     }}>
       
-      <a href="https://x.com/SignyHands" target="_blank" rel="noopener noreferrer" style={{ color: '#1DA1F2', display: 'flex', alignItems: 'center' }}>
+      <a href="https://x.com/SignyHands" target="_blank" rel="noopener noreferrer" style={{ color: '#1DA1F2', display: 'flex', alignItems: 'center', height: '40px'}}>
         <FontAwesomeIcon icon={faTwitter} style={{ marginRight: '5px' }} />
         Contact Us
       </a>
