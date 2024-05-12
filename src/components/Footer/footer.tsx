1d9c6c7311a469a2d4d810075387b650f6c32a40
Angel G. Ortiz <ortizangelg23@gmail.com>
2024-05-12 11:19:45 -0400

    made some changes to the footer styling. created the footer.css

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,25 +1,17 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons'; // Corrected icon import
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import styles from './footer.module.css'; // Import CSS module
 
 const Footer = () => {
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'flex-end',
-      alignItems: 'center',
-      height: '50px',
-      padding: '10px 20px',
-      background: '#f0f0f0',
-      borderTop: '1px solid #e1e1e1'
-    }}>
-      
-      <a href="https://x.com/SignyHands" target="_blank" rel="noopener noreferrer" style={{ color: '#1DA1F2', display: 'flex', alignItems: 'center', height: '40px'}}>
-        <FontAwesomeIcon icon={faTwitter} style={{ marginRight: '5px' }} />
+    <div className={styles.footerContainer}>
+      <a href="https://x.com/SignyHands" target="_blank" rel="noopener noreferrer" className={styles.footerLink}>
+        <FontAwesomeIcon icon={faTwitter} className={styles.footerIcon} />
         Contact Us
       </a>
     </div>
   )
 }
 
-export default Footer
+export default Footer;
